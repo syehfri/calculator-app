f697d86cb53fdfccca048775ce070c982b5bab63
24.11.6289 <yuanailosyehfri@students.amikom.ac.id>
2025-03-13 11:30:34 +0700

    penambahan fungsi penjumlahan dan pengurangan

---
+++ calculator.cs
@@ -0,0 +1,26 @@
+class calculator
+{
+	static void main (string[] args)
+	{
+		
+	int a = 10; 
+	int b = 6;
+	
+	console.writeline("hasil penambahan : {0} + {1} = {2}", a, b, penambahan(a, b));
+	console.writeline("hasil pengurangan : {0} - {1} = {2}", a, b, pengurangan(a, b));
+	
+	console.writeline("\nTekan sembarang key untuk keluar");
+	cosole.readkey();
+	}
+	static int penambahan (int a, int b)
+	{
+		return a + b;
+	}
+	static int pengurangan (int a, int b)
+	{
+		return a - b;
+		
+	}
+	
+}
+	
